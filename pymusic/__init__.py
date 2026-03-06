2d3ef510f3796210fa0b0fdaa6c5c730ed4c428d
Hangry Studios <siggeemilandersson@gmail.com>
2026-03-06 20:58:37 +0100

    refactor: proper package structure

---
+++ pymusic/__init__.py
@@ -0,0 +1,10 @@
+﻿from .note import Note, REST
+from .chord import Chord
+from .scale import Scale, SCALES
+from .track import Track
+from .song import Song
+from .rhythm import Pattern, beat, half, quarter, eighth, sixteenth
+from .synth import Synth
+from .export import export_wav, export_midi
+
+__version__ = "0.1.0"
